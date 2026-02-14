490669ec0a5268234f02730a9a999173ce824976
YuriPereiraFonseca <yuri.pereira.fonseca25@gmail.com>
2026-02-14 20:55:19 -0300

    criando uma interface para as validações

--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -10,11 +10,13 @@ import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 
 @Service
@@ -32,13 +34,15 @@ public class AdocaoService {
     @Autowired
     private AdocaoRepository repository;
 
+    @Autowired
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
 
     public void solicitar(SolicitacaoAdocaoDto dto) {
         Pet pet = petRepository.getReferenceById(dto.idPet());
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
-
-        //chamar validações aqui
+        validacoes.forEach(v -> v.validar(dto));
 
         Adocao adocao = new Adocao();
         adocao.setData(LocalDateTime.now());
